9614859d23a39d0fbcc4dd5c961efeca509c5e10
Kamavaram Harish <harishkamavram0007@gmail.com>
2026-08-01 21:00:45 +0530

    changed bakend url

--- frontend/src/services/api/instance.js
+++ frontend/src/services/api/instance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   // baseURL: 'http://localhost:8080',
     // baseURL: 'http://ec2-16-16-76-165.eu-north-1.compute.amazonaws.com',
-    baseURL: 'https://figured-bottle-morrison-elect.trycloudflare.com',
+    baseURL: 'https://previous-restricted-leslie-possibly.trycloudflare.com/',
   // baseURL:'http://192.168.1.111:8080',
   headers: {
     'Content-Type': 'application/json',
