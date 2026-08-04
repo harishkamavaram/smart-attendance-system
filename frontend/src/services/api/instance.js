dd5cc8bf05cd5ea35855e18b658f1e35490bcf4e
Kamavaram Harish <harishkamavram0007@gmail.com>
2026-08-04 21:38:12 +0530

    Testing url

--- frontend/src/services/api/instance.js
+++ frontend/src/services/api/instance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  // baseURL: "http://localhost:8080",
+  baseURL: "https://strictly-for-wagon-repairs.trycloudflare.com",
   // baseURL: 'http://ec2-16-16-76-165.eu-north-1.compute.amazonaws.com',
   // baseURL: 'https://previous-restricted-leslie-possibly.trycloudflare.com/',
   // baseURL:'https://endless-proudly-country-cartridge.trycloudflare.com',
